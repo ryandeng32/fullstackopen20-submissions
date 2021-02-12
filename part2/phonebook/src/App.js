edd208305aaa1a6e2720f126ae6ac6630b8d8192
Ryan Deng <ryan.deng32@gmail.com>
2021-02-12 11:28:03 -0500

    finish part2 b

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,40 +1,52 @@
 import React, { useState } from 'react';
-
+import Filter from './components/Filter';
+import PersonForm from './components/PersonForm';
+import Person from './components/Person';
 const App = () => {
-    const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+    const [persons, setPersons] = useState([
+        { name: 'Arto Hellas', number: '040-123456' },
+        { name: 'Ada Lovelace', number: '39-44-5323523' },
+        { name: 'Dan Abramov', number: '12-43-234345' },
+        { name: 'Mary Poppendieck', number: '39-23-6423122' },
+    ]);
     const [newName, setNewName] = useState('');
+    const [newNumber, setNewNumber] = useState('');
+    const [filter, setFilter] = useState('');
 
-    const handleClick = (e) => {
+    const display = filter
+        ? persons.filter((person) =>
+              person.name.toLowerCase().includes(filter.toLowerCase())
+          )
+        : persons;
+
+    const handleSubmit = (e) => {
         e.preventDefault();
+
         for (let i = 0; i < persons.length; i++) {
             if (persons[i].name === newName) {
                 window.alert(`${newName} is already added to phonebook`);
                 return;
             }
         }
-        setPersons(persons.concat({ name: newName }));
+        setPersons(persons.concat({ name: newName, number: newNumber }));
         setNewName('');
+        setNewNumber('');
     };
     return (
         <div>
             <h2>Phonebook</h2>
-            <form>
-                <div>
-                    <input
-                        value={newName}
-                        onChange={(e) => setNewName(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <button onClick={handleClick} type="submit">
-                        add
-                    </button>
-                </div>
-            </form>
+            <Filter filter={filter} setFilter={setFilter} />
+
+            <h2>add a new</h2>
+            <PersonForm
+                newName={newName}
+                newNumber={newNumber}
+                setNewName={setNewName}
+                setNewNumber={setNewNumber}
+                handleSubmit={handleSubmit}
+            />
             <h2>Numbers</h2>
-            {persons.map((person) => (
-                <p key={person.name}>{person.name}</p>
-            ))}
+            <Person persons={display} />
         </div>
     );
 };
---
+++ part2/phonebook/src/components/Person.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Person = ({ persons }) => {
+    return (
+        <>
+            {persons.map((person) => (
+                <p key={person.name}>
+                    {person.name} {person.number}
+                </p>
+            ))}
+        </>
+    );
+};
+
+export default Person;
