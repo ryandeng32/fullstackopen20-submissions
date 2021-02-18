53aa761d66bb0857d932886b5579b36cf63b36be
Ryan Deng <ryan.deng32@gmail.com>
2021-02-18 13:10:11 -0500

    finish until 3.6

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 let persons = [
     {
         id: 1,
@@ -27,10 +29,43 @@ app.get('/api/persons', (req, res) => {
     res.json(persons);
 });
 
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const person = persons.find((person) => person.id === id);
+    if (!person) {
+        return res.status(404).json({ error: 'no person found' });
+    }
+    res.json(person);
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);
+    persons = persons.filter((person) => person.id !== id);
+    res.status(204).end();
+});
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body;
+    if (!body.name || !body.number) {
+        return res.status(400).json({ error: 'no name or number provided' });
+    }
+    if (persons.find((person) => person.name === body.name)) {
+        return res.status(400).json({ error: 'name must be unique' });
+    }
+
+    const person = {
+        id: Math.floor(Math.random(1000) * 1000),
+        name: body.name,
+        number: body.number,
+    };
+    persons = persons.concat(person);
+    res.json(person);
+});
 app.get('/info', (req, res) => {
     res.send(`<p>phonebook has info for ${persons.length} people</p>
              <p>${new Date()}</P>`);
 });
+
 PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server started at port ${PORT}`);
